3ecdc45afd73dea33ba62456d2cd975db8e0b915
Maria <76159247+mariaSlimani@users.noreply.github.com>
2023-08-13 01:30:21 +0100

    Updating React + Fixing some warnings

--- dist/index.js
+++ dist/index.js
@@ -1,94 +1,151 @@
+var __extends =
+  (this && this.__extends) ||
+  (function () {
+    var extendStatics = function (d, b) {
+      extendStatics =
+        Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array &&
+          function (d, b) {
+            d.__proto__ = b
+          }) ||
+        function (d, b) {
+          for (var p in b)
+            if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]
+        }
+      return extendStatics(d, b)
+    }
+    return function (d, b) {
+      if (typeof b !== 'function' && b !== null)
+        throw new TypeError(
+          'Class extends value ' + String(b) + ' is not a constructor or null'
+        )
+      extendStatics(d, b)
+      function __() {
+        this.constructor = d
+      }
+      d.prototype =
+        b === null ? Object.create(b) : ((__.prototype = b.prototype), new __())
+    }
+  })()
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.Piece =
+  exports.allOptions =
+  exports.OptionContext =
+  exports.Option =
+  exports.AvatarStyle =
+  exports.Avatar =
+    void 0
 var PropTypes = require('prop-types')
 var React = require('react')
 var avatar_1 = require('./avatar')
 var options_1 = require('./options')
 var avatar_2 = require('./avatar')
+Object.defineProperty(exports, 'Avatar', {
+  enumerable: true,
+  get: function () {
+    return avatar_2.default
+  },
+})
+Object.defineProperty(exports, 'AvatarStyle', {
+  enumerable: true,
+  get: function () {
+    return avatar_2.AvatarStyle
+  },
+})
 var options_2 = require('./options')
+Object.defineProperty(exports, 'Option', {
+  enumerable: true,
+  get: function () {
+    return options_2.Option
+  },
+})
+Object.defineProperty(exports, 'OptionContext', {
+  enumerable: true,
+  get: function () {
+    return options_2.OptionContext
+  },
+})
+Object.defineProperty(exports, 'allOptions', {
+  enumerable: true,
+  get: function () {
+    return options_2.allOptions
+  },
+})
 var piece_1 = require('./avatar/piece')
-
-class AvatarComponent extends React.Component {
-  constructor(props) {
-    super(props)
-    this.optionContext = new options_1.OptionContext(options_1.allOptions)
-    this.updateOptionContext(props)
+var AvatarComponent = /** @class */ (function (_super) {
+  __extends(AvatarComponent, _super)
+  function AvatarComponent() {
+    var _this = (_super !== null && _super.apply(this, arguments)) || this
+    _this.optionContext = new options_1.OptionContext(options_1.allOptions)
+    return _this
   }
-
-  static getDerivedStateFromProps(nextProps) {
+  AvatarComponent.prototype.getChildContext = function () {
+    return { optionContext: this.optionContext }
+  }
+  AvatarComponent.prototype.UNSAFE_componentWillMount = function () {
+    this.updateOptionContext(this.props)
+  }
+  AvatarComponent.prototype.UNSAFE_componentWillReceiveProps = function (
+    nextProps
+  ) {
     this.updateOptionContext(nextProps)
-    return null
   }
-
-  updateOptionContext(props) {
-    var data = {}
-    for (var option of options_1.allOptions) {
-      var value = props[option.key]
-      if (value) {
-        data[option.key] = value
-      }
-    }
-    this.optionContext.setData(data)
-  }
-
-  render() {
-    var { avatarStyle, style, className } = this.props
+  AvatarComponent.prototype.render = function () {
+    var _a = this.props,
+      avatarStyle = _a.avatarStyle,
+      style = _a.style,
+      className = _a.className
     return React.createElement(avatar_1.default, {
       avatarStyle: avatarStyle,
       style: style,
       className: className,
     })
   }
-
-  render() {
-    var { avatarStyle, style, pieceType, pieceSize, viewBox } = this.props
-    return React.createElement(piece_1.default, {
-      avatarStyle: avatarStyle,
-      style: style,
-      pieceType: pieceType,
-      pieceSize: pieceSize,
-      viewBox: viewBox,
-    })
-  }
-
-  getChildContext() {
-    return { optionContext: this.optionContext }
-  }
-}
-
-AvatarComponent.childContextTypes = {
-  optionContext: PropTypes.instanceOf(options_1.OptionContext),
-}
-
-exports.Avatar = avatar_2.default
-exports.AvatarStyle = avatar_2.AvatarStyle
-exports.Option = options_2.Option
-exports.OptionContext = options_2.OptionContext
-exports.allOptions = options_2.allOptions
-
-class Piece extends React.Component {
-  constructor(props) {
-    super(props)
-    this.optionContext = new options_1.OptionContext(options_1.allOptions)
-    this.updateOptionContext(props)
-  }
-
-  static getDerivedStateFromProps(nextProps) {
-    this.updateOptionContext(nextProps)
-    return null
-  }
-
-  updateOptionContext(props) {
+  AvatarComponent.prototype.updateOptionContext = function (props) {
     var data = {}
-    for (var option of options_1.allOptions) {
+    for (
+      var _i = 0, allOptions_1 = options_1.allOptions;
+      _i < allOptions_1.length;
+      _i++
+    ) {
+      var option = allOptions_1[_i]
       var value = props[option.key]
-      if (value) {
-        data[option.key] = value
+      if (!value) {
+        continue
       }
+      data[option.key] = value
     }
     this.optionContext.setData(data)
   }
-
-  render() {
-    var { avatarStyle, style, pieceType, pieceSize, viewBox } = this.props
+  AvatarComponent.childContextTypes = {
+    optionContext: PropTypes.instanceOf(options_1.OptionContext),
+  }
+  return AvatarComponent
+})(React.Component)
+exports.default = AvatarComponent
+var Piece = /** @class */ (function (_super) {
+  __extends(Piece, _super)
+  function Piece() {
+    var _this = (_super !== null && _super.apply(this, arguments)) || this
+    _this.optionContext = new options_1.OptionContext(options_1.allOptions)
+    return _this
+  }
+  Piece.prototype.getChildContext = function () {
+    return { optionContext: this.optionContext }
+  }
+  Piece.prototype.UNSAFE_componentWillMount = function () {
+    this.updateOptionContext(this.props)
+  }
+  Piece.prototype.UNSAFE_componentWillReceiveProps = function (nextProps) {
+    this.updateOptionContext(nextProps)
+  }
+  Piece.prototype.render = function () {
+    var _a = this.props,
+      avatarStyle = _a.avatarStyle,
+      style = _a.style,
+      pieceType = _a.pieceType,
+      pieceSize = _a.pieceSize,
+      viewBox = _a.viewBox
     return React.createElement(piece_1.default, {
       avatarStyle: avatarStyle,
       style: style,
@@ -97,14 +154,25 @@ class Piece extends React.Component {
       viewBox: viewBox,
     })
   }
-
-  getChildContext() {
-    return { optionContext: this.optionContext }
+  Piece.prototype.updateOptionContext = function (props) {
+    var data = {}
+    for (
+      var _i = 0, allOptions_2 = options_1.allOptions;
+      _i < allOptions_2.length;
+      _i++
+    ) {
+      var option = allOptions_2[_i]
+      var value = props[option.key]
+      if (!value) {
+        continue
+      }
+      data[option.key] = value
+    }
+    this.optionContext.setData(data)
   }
-}
-
-Piece.childContextTypes = {
-  optionContext: PropTypes.instanceOf(options_1.OptionContext),
-}
-
+  Piece.childContextTypes = {
+    optionContext: PropTypes.instanceOf(options_1.OptionContext),
+  }
+  return Piece
+})(React.Component)
 exports.Piece = Piece
